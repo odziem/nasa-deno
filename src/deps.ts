0636fc864886f0577a01ccbd4f988f4df9280cc9
Adam Odziemkowski <adam@rangle.io>
2020-05-23 14:57:29 -0400

    Add versioning to deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,9 @@
 // Standard library dependencies
-export * as log from "https://deno.land/std/log/mod.ts";
+export * as log from "https://deno.land/std@0.53.0/log/mod.ts";
 
-export { join } from "https://deno.land/std/path/mod.ts";
-export { parse } from "https://deno.land/std/encoding/csv.ts";
-export { BufReader } from "https://deno.land/std/io/bufio.ts";
+export { join } from "https://deno.land/std@0.53.0/path/mod.ts";
+export { parse } from "https://deno.land/std@0.53.0/encoding/csv.ts";
+export { BufReader } from "https://deno.land/std@0.53.0/io/bufio.ts";
 
 // Third-party dependencies
-export { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
+export { Application, Router, send } from "https://deno.land/x/oak@v4.0.0/mod.ts";
