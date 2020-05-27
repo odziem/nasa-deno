7d6d4b0f013f05cd2e92512b9a45a70cbc8be30d
Adam Odziemkowski <adam@rangle.io>
2020-05-27 08:42:00 -0400

    Update planets model

--- src/deps.ts
+++ src/deps.ts
@@ -7,3 +7,5 @@ export { BufReader } from "https://deno.land/std@0.53.0/io/bufio.ts";
 
 // Third-party dependencies
 export { Application, Router, send } from "https://deno.land/x/oak@v4.0.0/mod.ts";
+
+export { pick } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
--- src/models/planets_model.ts
+++ src/models/planets_model.ts
@@ -1,23 +1,19 @@
-import { log, join, parse, BufReader } from "../deps.ts";
+import { pick, log, join, parse, BufReader } from "../deps.ts";
 
 interface Planet {
-  keplerName: string,
-  planetaryRadius: number,
-  planetCount: number,
-  stellarRadius: number,
-  stellarMass: number,
-  stellarTemperature: number,
-}
+  [ key : string ] : string
+};
 
-const planets = new Map<string, Planet>();
+let planets : Array<Planet>;
 
-async function loadPlanetData() {
-  const path = join(".", "data", "kepler_exoplanets_nasa.csv");
+export async function loadPlanetData() {
+  log.info("Loading planet data...");
   
+  const path = join(".", "data", "kepler_exoplanets_nasa.csv");
+
   const file = await Deno.open(path);
   const bufReader = new BufReader(file);
   
-  log.info("Loading planet data...");
   const result = await parse(bufReader, {
     header: true,
     comment: "#",
@@ -26,33 +22,32 @@ async function loadPlanetData() {
   // Close file resource id (rid) to avoid leaking resources.
   Deno.close(file.rid);
 
-  const matches = result.filter((item : any) => {
-    return item["koi_disposition"] === "CONFIRMED" 
-      && item["koi_steff"] > 5200 && item["koi_steff"] < 6200
-      && item["koi_srad"] > 0.9 && item["koi_srad"] < 1.1
-      && item["koi_smass"] > 0.9 && item["koi_smass"] < 1.1;
+  const planets = (result as Array<Planet>).filter((planet) => {
+    const planetaryRadius = Number(planet["koi_prad"]);
+    const stellarRadius = Number(planet["koi_srad"]);
+    const stellarMass = Number(planet["koi_smass"]);
+
+    return planet["koi_disposition"] === "CONFIRMED" 
+      && planetaryRadius > 0.5 && planetaryRadius < 1.5
+      && stellarRadius > 0.98 && stellarRadius < 1.02
+      && stellarMass > 0.78 && stellarMass < 1.04;
   });
 
-  matches.forEach((item : any) => {
-    planets.set(item["rowid"], {
-      keplerName: item["kepler_name"],
-      planetaryRadius: item["koi_prad"],
-      planetCount: item["koi_count"],
-      stellarMass: item["koi_smass"],
-      stellarRadius: item["koi_srad"],
-      stellarTemperature: item["koi_steff"],
-    });
-  });
+  return planets.map((planet) => {
+    return pick(planet, [
+      "kepler_name",
+      "koi_prad",
+      "koi_smass",
+      "koi_srad",
+      "koi_count",
+      "koi_steff"
+    ]);
+  })
 }
 
-await loadPlanetData();
+planets = await loadPlanetData();
+log.info(JSON.stringify(planets, null, 2));
 
 export function getAll() {
-  return Array.from(planets.values());
-};
-
-export function getOne(id : string) {
-  if (planets.has(id)) {
-    return planets.get(id);
-  }
+  return planets;
 };
