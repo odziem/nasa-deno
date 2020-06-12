3a02343c956a0284aec79bf5589a214acbd3c58e
Adam Odziemkowski <adam@rangle.io>
2020-06-12 02:04:27 -0400

    Update deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,18 +1,18 @@
 // Standard library dependencies
-export * as log from "https://deno.land/std@0.53.0/log/mod.ts";
+export * as log from "https://deno.land/std@0.56.0/log/mod.ts";
 
-export { join } from "https://deno.land/std@0.53.0/path/mod.ts";
-export { parse } from "https://deno.land/std@0.53.0/encoding/csv.ts";
-export { BufReader } from "https://deno.land/std@0.53.0/io/bufio.ts";
+export { join } from "https://deno.land/std@0.56.0/path/mod.ts";
+export { parse } from "https://deno.land/std@0.56.0/encoding/csv.ts";
+export { BufReader } from "https://deno.land/std@0.56.0/io/bufio.ts";
 
-// Third-party dependencies
+// Third party dependencies
 export {
   Application,
   Router,
   send,
-} from "https://deno.land/x/oak@v4.0.0/mod.ts";
+} from "https://deno.land/x/oak@v5.0.0/mod.ts";
 
 export {
+  pick,
   flatMap,
-  pick
 } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
