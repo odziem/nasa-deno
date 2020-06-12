b521800386350957ce24b5ca655cbff5f4300de0
Adam Odziemkowski <adam@rangle.io>
2020-06-12 02:10:27 -0400

    Update launches model

--- src/models/launches.ts
+++ src/models/launches.ts
@@ -1,13 +1,13 @@
-import { flatMap, log } from "../deps.ts";
+import { log, flatMap } from "../deps.ts";
 
 interface Launch {
-  upcoming: boolean;
-  success?: boolean;
   flightNumber: number;
-  launchDate: number;
   mission: string;
   rocket: string;
   customers: Array<string>;
+  launchDate: number;
+  upcoming: boolean;
+  success?: boolean;
   target?: string;
 }
 
@@ -15,30 +15,29 @@ const launches = new Map<number, Launch>();
 
 async function downloadLaunchData() {
   log.info("Downloading launch data...");
-  const response = await fetch("https://api.spacexdata.com/v3/launches");
+  const response = await fetch("https://api.spacexdata.com/v3/launches", {
+    method: "GET",
+  });
 
   if (!response.ok) {
-    log.warning("Failed to fetch SpaceX launches");
+    log.warning("Problem downloading launch data.");
     throw new Error("Launch data download failed.");
   }
 
   const launchData = await response.json();
-
-
   for (const launch of launchData) {
     const payloads = launch["rocket"]["second_stage"]["payloads"];
-
     const customers = flatMap(payloads, (payload: any) => {
       return payload["customers"];
     });
 
     const flightData = {
-      upcoming: launch["upcoming"],
-      success: launch['launch_success'],
       flightNumber: launch["flight_number"],
-      launchDate: launch["launch_date_unix"],
       mission: launch["mission_name"],
       rocket: launch["rocket"]["rocket_name"],
+      launchDate: launch["launch_date_unix"],
+      upcoming: launch["upcoming"],
+      success: launch["launch_success"],
       customers,
     };
 
@@ -47,27 +46,20 @@ async function downloadLaunchData() {
 }
 
 await downloadLaunchData();
+log.info(`Downloaded data for ${launches.size} SpaceX launches.`);
 
 export function getAll() {
   return Array.from(launches.values());
-};
+}
 
-export function getOne(id : number) {
+export function getOne(id: number) {
   if (launches.has(id)) {
     return launches.get(id);
   }
+  return null;
+}
 
-  throw new Error("Launch does not exist");
-};
-
-export function addOne (data : Launch) {
-  launches.set(data.flightNumber, Object.assign(data, {
-    upcoming: true,
-    customers: ["Zero to Mastery", "NASA"],
-  }));
-};
-
-export function removeOne(id : number) {
+export function removeOne(id: number) {
   const aborted = launches.get(id);
   if (aborted) {
     aborted.upcoming = false;
@@ -76,3 +68,12 @@ export function removeOne(id : number) {
   return aborted;
 }
 
+export function addOne(data: Launch) {
+  launches.set(
+    data.flightNumber,
+    Object.assign(data, {
+      upcoming: true,
+      customers: ["Zero to Mastery", "NASA"],
+    }),
+  );
+}
