25a5e935b5a61537d696da446e11c40d4baab0ad
Adam Odziemkowski <adam@rangle.io>
2020-06-12 02:16:14 -0400

    Update test

--- src/models/planets.test.ts
+++ src/models/planets.test.ts
@@ -1,42 +1,8 @@
-/**
- * Deno includes:
- * 
- * 1. The test runner in the CLI.
- * 2. Assertions in the standard library.
- * 3. Built-in test fixtures with Deno.test().
- */
-
 import {
   assertEquals,
-  assertNotEquals,
-} from "https://deno.land/std/testing/asserts.ts";
+} from "../test_deps.ts";
 
 import { filterHabitablePlanets } from "./planets.ts";
-
-const FALSE_POSITIVE = {
-  koi_disposition: "FALSE POSITIVE",
-};
-
-const PLANET_RADIUS_TOO_LARGE = {
-  koi_disposition: "CONFIRMED",
-  koi_prad: "1.5",
-  koi_srad: "1",
-  koi_smass: "1",
-};
-
-const STAR_RADIUS_TOO_LARGE = {
-  koi_disposition: "CONFIRMED",
-  koi_prad: "1",
-  koi_srad: "1.02",
-  koi_smass: "1",
-};
-
-const STAR_MASS_TOO_SMALL = {
-  koi_disposition: "CONFIRMED",
-  koi_prad: "1",
-  koi_srad: "1",
-  koi_smass: "0.78",
-};
 
 const HABITABLE_PLANET = {
   koi_disposition: "CONFIRMED",
@@ -45,13 +11,38 @@ const HABITABLE_PLANET = {
   koi_smass: "1",
 };
 
-Deno.test("match only habitable planets", () => {
+const NOT_CONFIRMED = {
+  koi_disposition: "FALSE POSITIVE",
+};
+
+const TOO_LARGE_PLANETARY_RADIUS = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1.5",
+  koi_srad: "1",
+  koi_smass: "1",
+};
+
+const TOO_LARGE_SOLAR_RADIUS = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1",
+  koi_srad: "1.02",
+  koi_smass: "1",
+};
+
+const TOO_LARGE_SOLAR_MASS = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1",
+  koi_srad: "1",
+  koi_smass: "1.04",
+};
+
+Deno.test("filter only habitable planets", () => {
   const filtered = filterHabitablePlanets([
-    FALSE_POSITIVE,
-    PLANET_RADIUS_TOO_LARGE,
-    STAR_RADIUS_TOO_LARGE,
-    STAR_MASS_TOO_SMALL,
-    HABITABLE_PLANET
+    HABITABLE_PLANET,
+    NOT_CONFIRMED,
+    TOO_LARGE_PLANETARY_RADIUS,
+    TOO_LARGE_SOLAR_RADIUS,
+    TOO_LARGE_SOLAR_MASS,
   ]);
 
   assertEquals(filtered, [
