2c6ca77bb3947997e02b7740c25726acaec32eba
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-12-28 08:02:17 -0500

    Update module versions

--- src/deps.ts
+++ src/deps.ts
@@ -1,16 +1,16 @@
 // Standard library dependencies
-export * as log from "https://deno.land/std@0.78.0/log/mod.ts";
+export * as log from "https://deno.land/std@0.119.0/log/mod.ts";
 
-export { join } from "https://deno.land/std@0.78.0/path/mod.ts";
-export { parse } from "https://deno.land/std@0.78.0/encoding/csv.ts";
-export { BufReader } from "https://deno.land/std@0.78.0/io/bufio.ts";
+export { join } from "https://deno.land/std@0.119.0/path/mod.ts";
+export { parse } from "https://deno.land/std@0.119.0/encoding/csv.ts";
+export { BufReader } from "https://deno.land/std@0.119.0/io/bufio.ts";
 
 // Third party dependencies
 export {
   Application,
   Router,
   send,
-} from "https://deno.land/x/oak@v6.3.2/mod.ts";
+} from "https://deno.land/x/oak@v10.1.0/mod.ts";
 
 export {
   pick,
--- src/test_deps.ts
+++ src/test_deps.ts
@@ -1,3 +1,3 @@
 export {
   assertEquals,
-} from "https://deno.land/std@0.78.0/testing/asserts.ts";
+} from "https://deno.land/std@0.119.0/testing/asserts.ts";
